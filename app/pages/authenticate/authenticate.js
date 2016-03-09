140cea89cfbb3f1ad77255a0355d9443b117dc9d
kantapong boonlert <kantapong boonlert>
2016-03-09 23:23:21 +0700

    Initial App

---
+++ app/pages/authenticate/authenticate.js
@@ -0,0 +1,11 @@
+import {Page} from 'ionic-angular';
+
+
+@Page({
+  templateUrl: 'build/pages/authenticate/authenticate.html'
+})
+export class AuthenticatePage {
+    constructor(){
+        this.logo = '/img/logo.png';
+    }
+}
